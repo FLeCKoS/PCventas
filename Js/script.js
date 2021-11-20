848a8ffb5ab15fdeabdd2b6595460734d317f39b
FLeCKoS <89850135+FLeCKoS@users.noreply.github.com>
2021-11-20 11:30:33 -0500

    20/11/2021
    cambio pagina

---
+++ Js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    console.log("pagina cargada!");
+    $("#btnlanding").on("click", function() {
+        $("#sectiontwo").show();
+
+
+    })
+    $("#idhome").on("click", function() {
+        $("#sectiontwo").hide();
+
+
+    })
+
+});
+
+function actionpage() {
+    $("#btnlanding").on("click", function() {
+
+    })
+}
